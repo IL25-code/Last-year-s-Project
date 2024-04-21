0e6796f386d261d889f73e84b8e8e1db6da3c99f
IL25-code <133204527+IL25-code@users.noreply.github.com>
2024-04-21 11:52:18 +0200

    Inizio della grafica

---
+++ login_page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login_page.css">
+    <title>Login Page</title>
+</head>
+<body>
+    <div class="main">
+        <input type="checkbox" id="chk" aria-hidden="true">
+        <div class="signup">
+            <form action="">
+                <label for="chk" aria-hidden="true">Sign Up</label>
+                <input type="text" name="txt" placeholder="Username" required="">
+                <input type="text" name="email" placeholder="E-mail" required="">
+                <input type="text" name="password" placeholder="Password" required="">
+                <button>Sign Up</button>
+            </form>
+        </div>
+
+        <div class="login">
+            <form action="">
+                <label for="chk" aria-hidden="true">Login</label>
+                <input type="text" name="email" placeholder="E-mail" required="">
+                <input type="text" name="password" placeholder="Password" required="">
+                <button>Login</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
